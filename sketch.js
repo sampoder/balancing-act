566812624fe991a02bd13fbf8849778900f01daa
Sam Poder <sam.r.poder@gmail.com>
2025-03-23 18:16:01 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,38 @@
-/*
- * 👋 Hello! This is an ml5.js example made and shared with ❤️.
- * Learn more about the ml5.js project: https://ml5js.org/
- * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
- *
- * This example demonstrates drawing skeletons on poses for the MoveNet model.
- */
-
 let video;
 let bodyPose;
 let poses = [];
 let connections;
 
-let predefined = ["💼", "🎓", "❤️", "📚", "📝", "⏳", "🏃‍♂️", "📆", "🧑‍🧑‍🧒‍🧒", "🗳️"];
+let predefined = ["💼", "🎓", "❤️", "📚", "📝", "⏳", "🏃‍♂️", "📆", "🧑‍🧑‍🧒‍🧒", "🗳️"]; // while we're waiting for emojis to load from the DB we use these
 
 function preload() {
-  // Load the bodyPose model
-  bodyPose = ml5.bodyPose();
+  bodyPose = ml5.bodyPose(); // Load the bodyPose model: https://docs.ml5js.org/#/reference/bodypose
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // fill the screen
 
-  // Create the video and hide it
+  // Create the video and hide it, because 
   video = createCapture(VIDEO);
   video.size(windowWidth, windowHeight);
   video.hide();
 
   // Start detecting poses in the webcam video
   bodyPose.detectStart(video, r => poses = r);
-  // Get the skeleton connection information
   connections = bodyPose.getSkeleton();
   
-  world.gravity.y = 10;
+  world.gravity.y = 10; // this is how the balls start falling
   
   colours = [
     color(130, 211, 27),
     color(255, 204, 0),
     color(18, 150, 224),
     color(252, 183, 27)
-  ]
+  ] // randomise the background colour
   
   lines = new Group()
   
-  floor = new Sprite(0, windowHeight -2, windowWidth * 2, 5, 'static');
+  floor = new Sprite(0, windowHeight -2, windowWidth * 2, 5, 'static'); // this is how we stop the balls from falling through the floor!
   
   stressors = ["💼", "🎓", "❤️", "📚", "📝", "⏳", "🏃‍♂️", "📆", "🧑‍🧑‍🧒‍🧒", "🗳️"]
 }
@@ -56,27 +46,27 @@ let lines;
 let stressors;
 
 function draw() {  
-  if(poses.length == 0 && !flag) {
+  if(poses.length == 0 && !flag) { // change the colour of the background when there's no one in the frame
     c = random(colours);
     flag = true;
   } else if (poses.length > 0) {
     flag = false;
   }
-  if(random(100) > 50) {
+  if(random(100) > 50) { // maybe we draw a ball?
     ball = new Sprite(random(windowWidth), 0, 100);
     ball.textSize = 40;
-    ball.text = random(stressors);
+    ball.text = random(stressors); // random emoji!
     ball.collides(lines, ball => {
-      ball.vel.y = -45;
+      ball.vel.y = -45; // bounce up if it hits a skeleton line
     })
   }
   
   background(c);
   
   for (let i = 0; i < lines.length; i++) {
-    lines[i].remove()
+    lines[i].remove() // redraw the skeleton everytime
   }
-  // Draw the skeleton connections
+  // draw the skeleton
   for (let i = 0; i < poses.length; i++) {
     let pose = poses[i];
     for (let j = 0; j < connections.length; j++) {
@@ -84,7 +74,7 @@ function draw() {
       let pointBIndex = connections[j][1];
       let pointA = pose.keypoints[pointAIndex];
       let pointB = pose.keypoints[pointBIndex];
-      // Only draw a line if both points are confident enough
+      // only draw a line if both points are confident enough
       if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
         stroke(0, 0, 0);
         strokeWeight(6);
@@ -96,6 +86,7 @@ function draw() {
   }
 }
 
+// we call this every 10 seconds
 async function updateStressors() {
   stressors = [...predefined, ...(await fetch("https://api2.hackclub.com/v0.1/SlashZSamPoder/Submissions")
                     .then(r => r.json())
